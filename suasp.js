db72109c7b7c7d89a9bb14b17146485d363c42b1
quochuy2007 <138018915+quochuy2007@users.noreply.github.com>
2024-01-03 20:57:56 +0700

    Add files via upload

---
+++ suasp.js
@@ -0,0 +1,36 @@
+const image=document.querySelector('#image')
+const name=document.querySelector('#name')
+const year=document.querySelector('#year')
+const price=document.querySelector('#price')
+const km=document.querySelector('#km')
+const button=document.querySelector('#Signinbutton')
+button.addEventListener('click',()=>{
+    const imageValue=image.value;
+    const nameValue=name.value;
+    const yearValue=year.value;
+    const priceValue=price.value;
+    const kmValue=km.value;
+    const product={
+        image:imageValue,
+        name:nameValue,
+        year:yearValue,
+        price:priceValue,
+        km:kmValue,
+    }
+    fetch('https://655a17166981238d054d1b0d.mockapi.io/api/huy/Car/1', {
+  method: 'PUT',
+  headers: {'content-type':'application/json'},
+  // Send your data in the request body as JSON
+  body: JSON.stringify(product)
+}).then(res => {
+  if (res.ok) {
+      return res.json();
+  }
+  // handle error
+}).then(task => {
+  alert ("Sửa sản phẩm thành công")
+  // do something with the new task
+}).catch(error => {
+  // handle error
+})
+} )
